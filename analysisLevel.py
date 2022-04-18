2590ca2f4e9f39abf61b88955311c34d73420483
Julien Godfroid <kragg77@hotmail.com>
2022-04-18 12:19:53 -0400

    Add files via upload

--- analysisLevel.py
+++ analysisLevel.py
@@ -2,6 +2,7 @@ import cast_upgrade_1_6_13 # @UnusedImport
 import cast.analysers.dotnet
 from cast.analysers import log
 import re
+import os
 
 class MissingImportsAnalysis(cast.analysers.dotnet.Extension): 
     def __init__(self):        
@@ -18,15 +19,25 @@ class MissingImportsAnalysis(cast.analysers.dotnet.Extension):
     def start_file(self, _file):
         if _file.get_path() != '':
             log.info('Processing %s ...' % _file.get_path().lower())
+            
+            _, ext = os.path.splitext(_file.get_path())
+            if not ext in ['.cs','.vb']:
+                return
+
             lineNb = 0
             with open(_file.get_path(), 'r') as f:
                 for line in f:
                     lineNb +=1
                     #using System.Collections.Generic;
-                    matchObj = re.search('^[\s]*using[\s]+([\w\.]+)', line)
+                    matchObj = re.search('^[\s]*using[\s]+([\w\.]+)[\s]*;', line)
                     if matchObj: #import
                         log.info('Import: %s' % matchObj.group(1))
                         self.intermediateFile.write('%s;%s\n' % (_file.get_path().lower(), matchObj.group(1)))
                         self.NbImports += 1
-     
+                    #using alias = System.Collections.Generic;
+                    matchObj = re.search('^[\s]*using[\s]+[\w\.]+[\s]=[\s]*([\w\.]+)[\s]*;', line)
+                    if matchObj: #import
+                        log.info('Import: %s' % matchObj.group(1))
+                        self.intermediateFile.write('%s;%s\n' % (_file.get_path().lower(), matchObj.group(1)))
+                        self.NbImports += 1
             
